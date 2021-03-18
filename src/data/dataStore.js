27b5c9390444c682943ffaa2df008b25a81fbf02
Anka <kozlowskanka@gmail.com>
2021-03-18 21:24:23 +0100

    Correct descriptions

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: 'Anna Bartoszko',
-  phone: '605 657 338',
+  phone: '+ 48 605 657 338',
   mail: 'ania.bartoszko@gmail.com',
-  description: 'Presented projects show a variety of design activities. My portfolio consists of diverse works showing numerous techniques and tools that I use. This site was completely designed and coded by me using React.',
+  description: 'Presented projects show a variety of design activities. My portfolio consists of diverse works showing numerous techniques and tools that I use. This site was completely developed by me - starting with user interface designed with Figma to the final mechanism of site made with React JavaScript library.',
 };
